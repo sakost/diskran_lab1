319a38f0159407db78344c31de13ab0a6c00ec4c
sakost <sakost01@gmail.com>
2020-10-05 01:57:53 +0300

    add normal benchmark

--- benchmark.cpp
+++ benchmark.cpp
@@ -37,7 +37,16 @@ int main() {
     NSort::BucketSort(v);
     finish = std::chrono::steady_clock::now();
     dur = finish - start;
-    std::cerr << "sort " << std::chrono::duration_cast<std::chrono::milliseconds>(dur).count() << " ms" << std::endl;
+    std::cerr << "custom bucket sort " << std::chrono::duration_cast<std::chrono::milliseconds>(dur).count() << " ms" << std::endl;
+
+    std::shuffle(v.Begin(), v.End());
+
+    start = std::chrono::steady_clock::now();
+    std::stable_sort(v.Begin(), v.End());
+    finish = std::chrono::steady_clock::now();
+
+    dur = finish - start;
+    std::cerr << "stable sort from std " << std::chrono::duration_cast<std::chrono::milliseconds>(dur).count() << " ms" << std::endl;
 
     start = std::chrono::steady_clock::now();
     for (size_t i = 0; i < v.Size(); i++) {
